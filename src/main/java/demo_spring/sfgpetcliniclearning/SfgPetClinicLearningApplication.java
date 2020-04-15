64dff34ae42c5aecdc23a6e64ff5c63e12317f88
bajanf <flavia.bajan@gmail.com>
2020-04-15 14:25:30 +0300

    Initial Project Add

---
+++ src/main/java/demo_spring/sfgpetcliniclearning/SfgPetClinicLearningApplication.java
@@ -0,0 +1,13 @@
+package demo_spring.sfgpetcliniclearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicLearningApplication.class, args);
+    }
+
+}
